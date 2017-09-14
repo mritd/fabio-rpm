09398492a83e4b671b4c2e780629b43b8ae98835
mritd <mritd1234@gmail.com>
2017-09-14 11:33:47 +0800

    去除家目录设置

--- rpmscripts/preinstall.sh
+++ rpmscripts/preinstall.sh
@@ -1,2 +1,2 @@
 getent group fabio >/dev/null || groupadd -r fabio
-getent passwd fabio >/dev/null || useradd -r -g fabio -d /var/lib/fabio -s /sbin/nologin -c "fabio user" fabio
+getent passwd fabio >/dev/null || useradd -r -g fabio -M -s /sbin/nologin -c "fabio user" fabio
